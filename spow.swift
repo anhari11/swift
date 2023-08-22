9463af11620d909c4260db6af053a936d62e63ac
anhari11 <118302945+anhari11@users.noreply.github.com>
2023-08-22 12:10:27 +0200

    Add files via upload
    Thats what I practiced  today, the concepts of variables and constants,  how to treat with arrays and for loops.

---
+++ spow.swift
@@ -0,0 +1,60 @@
+/*
+   let score = 52
+
+
+
+print (score)
+
+var ages = [12, 3, 5, 7, 9, 12, 4]
+
+
+
+let numberofitems = ages.count
+print (numberofitems)
+
+var disorder = [12, 3, 5, 7, 9, 12, 4]
+
+let ordered  = disorder.sorted()
+let disorde = disorder.reversed()
+print (ordered)
+print (disorde)
+
+var ags = [2, 6, 34, 55, 12, 1, 0, 9]
+ags.reverse()
+print (ags)
+
+var agesSet = Set(ags)
+let result = agesSet.contains(33)
+print (result)
+func add(number: Int, second: Int)  {
+    let sum = number + second
+    print (sum)
+}
+
+add(number: 12, second: 3)
+var player1score = 50
+var player2score = 49
+
+if player1score > player2score {
+    print("Player 1 won")
+} else if player1score == player2score {
+    print("Equal")
+} else {
+    print("Tfu")
+}
+
+var players0 = [4, 5, 5, 3, 2, 6, 7, 8]
+
+for player in ran {
+    print (player)
+
+
+}
+
+
+
+*/
+
+
+
+
